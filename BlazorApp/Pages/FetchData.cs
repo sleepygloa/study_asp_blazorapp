423888e57b49d3183cddddcdc1432d10e9f4ba97
sleepygloa <sleepygloa@gmail.com>
2023-02-20 11:09:25 +0900

    study templated Component

---
+++ BlazorApp/Pages/FetchData.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using BlazorApp.Data;
+using System.Threading.Tasks;
+
+namespace BlazorApp.Pages
+{
+	public partial class FetchData
+	{
+		private WeatherForecast[] forcasts;
+
+		protected override async Task OnInitializeAsync()
+		{
+			forcasts = await WeatherForecastService.GetForecastAsync(DateTime.Now);
+		}
+	}
+}
+
